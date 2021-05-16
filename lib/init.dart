4d37ab5ac648d593c817fb6ca90eb88d9cece6a5
Ldev <kanhadubey2@gmail.com>
2021-05-16 18:44:15 +0530

    Fixed init flow and data redundancy checks

--- lib/init.dart
+++ lib/init.dart
@@ -1,11 +1,9 @@
-import 'package:cookbook_app/constants.dart';
-import 'package:cookbook_app/pages/edit_profile.dart';
+import 'package:cookbook_app/screens/auth/auth_one.dart';
+import 'package:cookbook_app/screens/home/home_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:cookbook_app/screens/home/home_screen.dart';
-import 'package:cookbook_app/screens/auth/auth_one.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:nb_utils/nb_utils.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Initialise extends StatelessWidget {
   @override
@@ -27,25 +25,6 @@ class Initialise extends StatelessWidget {
 
     if ((_prefs.getBool('loggedIn') ?? false)) {
       return HomeScreen().launch(
-        context,
-        isNewTask: true,
-      );
-    } else if (!_prefs.getString('username').isEmptyOrNull) {
-      return EditProfilePage(
-        Constants.setupTitleForProfilePage,
-      ).launch(
-        context,
-        isNewTask: true,
-      );
-    } else if (!_prefs.getString('email').isEmptyOrNull) {
-      return EditProfilePage(
-        Constants.setupTitleForProfilePage,
-      ).launch(
-        context,
-        isNewTask: true,
-      );
-    } else if ((_prefs.getBool('loggedIn') ?? false)) {
-      return AuthOne().launch(
         context,
         isNewTask: true,
       );
--- lib/screens/auth/firebase_methods.dart
+++ lib/screens/auth/firebase_methods.dart
@@ -13,75 +13,84 @@ Future<void> checkProfileExistsOrNot(BuildContext context, {bool filterByMobNo, 
 
   SharedPreferences _prefs = await SharedPreferences.getInstance();
 
-  if (!filterByMobNo) {
-    qSnap = await usrs.where('email', isEqualTo: email).get();
-    if (qSnap.docs.length > 0) {
-      if (qSnap.docs[0].data()['name'] != '' &&
-          qSnap.docs[0].data()['email'] != '' &&
-          qSnap.docs[0].data()['pfpUrl'] != '') {
-        _prefs.setString('username', qSnap.docs[0].data()['name']);
-        _prefs.setString('email', qSnap.docs[0].data()['email']);
-        _prefs.setString('pfpUrl', qSnap.docs[0].data()['pfp_url']);
-        HomeScreen().launch(context, isNewTask: true);
-      } else if (qSnap.docs[0].data()['name'] != '' && qSnap.docs[0].data()['email'] != '') {
-        _prefs.setString('username', qSnap.docs[0].data()['name']);
-        _prefs.setString('email', qSnap.docs[0].data()['email']);
-        EditProfilePage(Constants.setupTitleForProfilePage).launch(context, isNewTask: true);
-      } else if (qSnap.docs[0].data()['email' != ''] && qSnap.docs[0].data()['pfp_url'] != '') {
-        _prefs.setString('email', qSnap.docs[0].data()['email']);
-        _prefs.setString('pfpUrl', qSnap.docs[0].data()['pfp_url']);
-        EditProfilePage(Constants.setupTitleForProfilePage).launch(context, isNewTask: true);
-      } else if (qSnap.docs[0].data()['pfp_url'] != '' && qSnap.docs[0].data()['name'] != '') {
-        _prefs.setString('pfpUrl', qSnap.docs[0].data()['pfp_url']);
-        _prefs.setString('username', qSnap.docs[0].data()['name']);
-        EditProfilePage(Constants.setupTitleForProfilePage).launch(context, isNewTask: true);
+  try {
+    if (!filterByMobNo) {
+      qSnap = await usrs.where('email', isEqualTo: email).get();
+      if (qSnap.docs.length > 0) {
+        _prefs.setString('uid', qSnap.docs[0].id);
+        if (!qSnap.docs[0].data()['name'].toString().isEmptyOrNull &&
+            !qSnap.docs[0].data()['email'].toString().isEmptyOrNull &&
+            !qSnap.docs[0].data()['pfpUrl'].toString().isEmptyOrNull) {
+          print(qSnap.docs[0].data());
+          _prefs.setString('username', qSnap.docs[0].data()['name']);
+          _prefs.setString('email', qSnap.docs[0].data()['email']);
+          _prefs.setString('pfpUrl', qSnap.docs[0].data()['pfp_url']);
+          HomeScreen().launch(context, isNewTask: true);
+        } else if (!qSnap.docs[0].data()['name'].toString().isEmptyOrNull &&
+            !qSnap.docs[0].data()['email'].toString().isEmptyOrNull) {
+          _prefs.setString('username', qSnap.docs[0].data()['name']);
+          _prefs.setString('email', qSnap.docs[0].data()['email']);
+          EditProfilePage(Constants.setupTitleForProfilePage).launch(context, isNewTask: true);
+        } else if (!qSnap.docs[0].data()['email'].toString().isEmptyOrNull &&
+            !qSnap.docs[0].data()['pfp_url'].toString().isEmptyOrNull) {
+          _prefs.setString('email', qSnap.docs[0].data()['email']);
+          _prefs.setString('pfpUrl', qSnap.docs[0].data()['pfp_url']);
+          EditProfilePage(Constants.setupTitleForProfilePage).launch(context, isNewTask: true);
+        } else if (!qSnap.docs[0].data()['pfp_url'].toString().isEmptyOrNull &&
+            !qSnap.docs[0].data()['name'].toString().isEmptyOrNull) {
+          _prefs.setString('pfpUrl', qSnap.docs[0].data()['pfp_url']);
+          _prefs.setString('username', qSnap.docs[0].data()['name']);
+          EditProfilePage(Constants.setupTitleForProfilePage).launch(context, isNewTask: true);
+        } else {
+          EditProfilePage(Constants.setupTitleForProfilePage).launch(context, isNewTask: true);
+        }
       } else {
-        EditProfilePage(Constants.setupTitleForProfilePage).launch(context, isNewTask: true);
+        EditProfilePage(
+          Constants.setupTitleForProfilePage,
+          newUser: true,
+          mobNo: mobNo,
+        ).launch(context, isNewTask: true);
       }
     } else {
-      EditProfilePage(
-        Constants.setupTitleForProfilePage,
-        newUser: true,
-        mobNo: mobNo,
-      ).launch(context, isNewTask: true);
-    }
-  } else {
-    qSnap = await usrs.where('mobNo', isEqualTo: mobNo).get();
-    if (qSnap.docs.length > 0) {
-      if (qSnap.docs[0].data()['name'] != '' &&
-          qSnap.docs[0].data()['mobNo'] != '' &&
-          qSnap.docs[0].data()['pfpUrl'] != '') {
-        _prefs.setString('username', qSnap.docs[0].data()['name']);
-        _prefs.setString('mobNo', qSnap.docs[0].data()['mobNo']);
-        _prefs.setString('pfpUrl', qSnap.docs[0].data()['pfp_url']);
-        HomeScreen().launch(context, isNewTask: true);
-      } else if (qSnap.docs[0].data()['name'] != '' && qSnap.docs[0].data()['mobNo'] != '') {
-        _prefs.setString('username', qSnap.docs[0].data()['name']);
-        _prefs.setString('mobNo', qSnap.docs[0].data()['mobNo']);
-        EditProfilePage(Constants.setupTitleForProfilePage).launch(context, isNewTask: true);
-      } else if (qSnap.docs[0].data()['mobNo' != ''] && qSnap.docs[0].data()['pfp_url'] != '') {
-        _prefs.setString('mobNo', qSnap.docs[0].data()['email']);
-        _prefs.setString('pfpUrl', qSnap.docs[0].data()['pfp_url']);
-        EditProfilePage(Constants.setupTitleForProfilePage).launch(context, isNewTask: true);
-      } else if (qSnap.docs[0].data()['pfp_url'] != '' && qSnap.docs[0].data()['name'] != '') {
-        _prefs.setString('pfpUrl', qSnap.docs[0].data()['pfp_url']);
-        _prefs.setString('username', qSnap.docs[0].data()['name']);
-        EditProfilePage(Constants.setupTitleForProfilePage).launch(context, isNewTask: true);
+      qSnap = await usrs.where('mobNo', isEqualTo: mobNo).get();
+      if (qSnap.docs.length > 0) {
+        _prefs.setString('uid', qSnap.docs[0].id);
+        if (!qSnap.docs[0].data()['name'].toString().isEmptyOrNull &&
+            !qSnap.docs[0].data()['mobNo'].toString().isEmptyOrNull &&
+            !qSnap.docs[0].data()['pfpUrl'].toString().isEmptyOrNull) {
+          _prefs.setString('username', qSnap.docs[0].data()['name']);
+          _prefs.setString('mobNo', qSnap.docs[0].data()['mobNo']);
+          _prefs.setString('pfpUrl', qSnap.docs[0].data()['pfp_url']);
+          HomeScreen().launch(context, isNewTask: true);
+        } else if (!qSnap.docs[0].data()['name'].toString().isEmptyOrNull &&
+            !qSnap.docs[0].data()['mobNo'].toString().isEmptyOrNull) {
+          _prefs.setString('username', qSnap.docs[0].data()['name']);
+          _prefs.setString('mobNo', qSnap.docs[0].data()['mobNo']);
+          EditProfilePage(Constants.setupTitleForProfilePage).launch(context, isNewTask: true);
+        } else if (!qSnap.docs[0].data()['mobNo'.toString().isEmptyOrNull] &&
+            !qSnap.docs[0].data()['pfp_url'].toString().isEmptyOrNull) {
+          _prefs.setString('mobNo', qSnap.docs[0].data()['email']);
+          _prefs.setString('pfpUrl', qSnap.docs[0].data()['pfp_url']);
+          EditProfilePage(Constants.setupTitleForProfilePage).launch(context, isNewTask: true);
+        } else if (!qSnap.docs[0].data()['pfp_url'].toString().isEmptyOrNull &&
+            !qSnap.docs[0].data()['name'].toString().isEmptyOrNull) {
+          _prefs.setString('pfpUrl', qSnap.docs[0].data()['pfp_url']);
+          _prefs.setString('username', qSnap.docs[0].data()['name']);
+          EditProfilePage(Constants.setupTitleForProfilePage).launch(context, isNewTask: true);
+        } else {
+          EditProfilePage(Constants.setupTitleForProfilePage).launch(context, isNewTask: true);
+        }
       } else {
-        EditProfilePage(Constants.setupTitleForProfilePage).launch(context, isNewTask: true);
+        EditProfilePage(
+          Constants.setupTitleForProfilePage,
+          newUser: true,
+          mobNo: mobNo,
+        ).launch(context, isNewTask: true);
       }
-    } else {
-      EditProfilePage(
-        Constants.setupTitleForProfilePage,
-        newUser: true,
-        mobNo: mobNo,
-      ).launch(context, isNewTask: true);
     }
+  } catch (e, s) {
+    print(e.toString() + s.toString());
   }
 
   return;
-}
-
-checkDuplicacy(String email) async {
-  QuerySnapshot qSnap = await fs.collection('users').get();
 }
